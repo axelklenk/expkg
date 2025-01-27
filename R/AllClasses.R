0aa47cb32b34f62273de8b5db0fab63865bd30eb
Axel Klenk <axel.klenk@gmail.com>
2025-01-27 17:14:25 +0100

    further minimized package according to advice by Martin Maechler

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,31 +1,3 @@
-
-#' @import methods
-NULL
-
-#' `A` class
-#'
-#' A test class, superclass of test class `C`.
-#'
-#' This class is part of a pair of classes for testing an issue with S4
-#' method dispatch.
-#'
-#' @aliases A
-#' @name A-class
-#' @rdname A-class
-#' @exportClass A
 setClass("A")
-
-
-#' `C` class
-#'
-#' A test class, subclass of test class `A`.
-#'
-#' This class is part of a pair of classes for testing an issue with S4
-#' method dispatch.
-#'
-#' @aliases C
-#' @name C-class
-#' @rdname C-class
-#' @exportClass C
 setClass("C", contains=c("A"))
 
--- README.md
+++
@@ -1,5 +0,0 @@
-# expkg: an example package for exploring and testing an S4 method dispatch issue
-
-The `expkg` is made for exploring an issue with S4 method dispatch that is observed in
-package `GSVA` but cannot be replicated using a simple script, i.e. without the package
-structure.
